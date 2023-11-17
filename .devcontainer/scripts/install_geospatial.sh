f57a99535ec1246a6109d8d517cbfc01d22a6b07
K. Arthur Endsley <endsley@umich.edu>
2023-11-17 14:43:48 -0700

    Added .devcontainer

---
+++ .devcontainer/scripts/install_geospatial.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Modified from original in rocker2 project; see licensing information here:
+#   https://github.com/rocker-org/rocker-versioned2/blob/master/LICENSE
+
+set -e
+
+# always set this for scripts but don't declare as ENV..
+export DEBIAN_FRONTEND=noninteractive
+
+## build ARGs
+NCPUS=${NCPUS:--1}
+
+# a function to install apt packages only if they are not installed
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
+apt_install \
+    gdal-bin \
+    lbzip2 \
+    libgdal-dev \
+    libgeos-dev \
+    libgsl0-dev \
+    libhdf4-alt-dev \
+    libhdf5-dev \
+    libjq-dev \
+    libpq-dev \
+    libproj-dev \
+    libnetcdf-dev \
+    libssl-dev \
+    libudunits2-dev \
+    netcdf-bin \
+    protobuf-compiler \
+    tk-dev \
+    unixodbc-dev
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+rm -r /tmp/downloaded_packages
